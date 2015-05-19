2587c9ef98592a30b7f1c880e8d6794fc9f9e314
pierre <pierre-lin.bonnemaison@y-nov.com>
2015-05-19 19:31:00 +0200

    Ajout d'un tablea d'exemple dans la création de base

--- createDatabase.sql
+++ createDatabase.sql
@@ -1,3 +1,12 @@
 #Fichier de création de la base
 CREATE DATABASE IF NOT EXISTS descartes_example;
 use descartes_example;
+
+-- Create table of exemple
+CREATE TABLE IF NOT EXISTS exemple
+(
+	id INT NOT NULL AUTO_INCREMENT,
+	clef VARCHAR(25) NOT NULL,
+	valeur VARCHAR(255) NOT NULL,
+	PRIMARY KEY (id)
+);
