f2b50ce7d68d5803a8e218350a4c8fa70116fb7e
Pierre-Lin Bonnemaison <pierre.lin@free.fr>
2015-07-27 17:05:00 +0200

    Mise à jour du système de cache, ajout du cache custom

--- controllers/index.php
+++ controllers/index.php
@@ -7,11 +7,19 @@ class index extends Controller
 	//Pour ajouter du cache, ajouter un attribut :
 	//public $cache_nomMethode = durée_cache_en_minute;
 
+	//Pour ajouter un cache custom, ajouter une methode :
+	//public function _cache_custom_nomMethod() { return $yourCustomTraitements }
+
 	/**
 	 * Page d'index par défaut
 	 */	
 	public function byDefault()
 	{
 		return $this->render("index");
+	}
+
+	public function toto()
+	{
+		echo uniqid();
 	}
 }
