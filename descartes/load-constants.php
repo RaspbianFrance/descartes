abec6b95bd7e46b93301c085585c3489d8422333
OsaAjani <pierre.lin@free.fr>
2016-05-12 16:28:41 +0200

    Modification dossier app pour descartes et meilleur gestion des constantes et de leur chargement.

---
+++ descartes/load-constants.php
@@ -0,0 +1,22 @@
+<?php
+	//On va inclure les constantes fixes
+	require_once('../constants.php');
+
+	//0n va inclure les constantes customisable
+	require_once(PWD . 'constants.php');
+
+	//On va inclure les constantes de tous les modules (dans l'ordre du dossier)
+	foreach (scandir(PWD_MODULES) as $filename)
+	{
+		if ($filename == '.' || $filename == '..')
+		{
+			continue;
+		}
+		
+		if (!file_exists(PWD_MODULES . $filename . '/constants.php'))
+		{
+			continue;
+		}
+
+		require_once(PWD_MODULES . $filename . '/constants.php');
+	}
